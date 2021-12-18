1b547f13f38a1ad8825a019ebdb7ddbadf8cf4e8
Ervis Lapi <lervis87@gmail.com>
2021-12-18 11:57:50 +0000

    added lists operations

--- 08_Lists/08_05_strings_lists.py
+++ 08_Lists/08_05_strings_lists.py
@@ -1,4 +1,18 @@
 # strings and lists
+# convert all chars in list
 a = "spam"
-b = list(a)
+b = list(a) # converts all letters in items of array
 print(b)
+
+# split (using space)
+a = "spam spam spam"
+b = a.split()
+print(b)
+
+a = "spam-spam-spam"
+b = a.split("-") #using delimiter
+print(b)
+
+# list to string with joint
+print('"-".join')
+print(("-").join(b)) #this will convert list in string
