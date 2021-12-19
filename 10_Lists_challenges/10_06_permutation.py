8226fdef538676f38ddd74112d518c0171ac8015
Ervis Lapi <lervis87@gmail.com>
2021-12-19 18:32:02 +0000

    permutations done right

--- 10_Lists_challenges/10_06_permutation.py
+++ 10_Lists_challenges/10_06_permutation.py
@@ -1,20 +1,21 @@
 # 6 Permutation - same characters but in different orders
 
-list_1 = [1,3,2]
+list_1 = [4,3,2]
 list_2 = [2,3,1]
 
+# First method - iterating through list 1
 def check_permutation(list_1,list_2):
     if len(list_1) != len(list_2):
         return False
-    output = False
     for letter in list_1:
         if letter in list_2:
-            list_2.pop(letter)
+            output = True
         else:
-            output = False
-    return True
+            return False
+    return output
 print(check_permutation(list_1,list_2))
 
+# second method, sorting lists and comparing them
 def check_permutation_2(list_1,list_2):
     if len(list_1) != len(list_2):
         return False
