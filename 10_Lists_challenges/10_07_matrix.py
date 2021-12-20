b51ed2d5255f37c5ae31427d2fd20716d88b32f5
Ervis Lapi <lervis87@gmail.com>
2021-12-20 19:49:18 +0000

    working on matrix rotation again

--- 10_Lists_challenges/10_07_matrix.py
+++ 10_Lists_challenges/10_07_matrix.py
@@ -1,4 +1,5 @@
 # Rotat a matrix by 90 degres
+import numpy as np
 
 matr = [
     [1,2,3],
@@ -6,14 +7,24 @@ matr = [
     [7,8,9]
 ]
 
-def rotate_matrix(matr):
-    print(matr)
-    for i in range(0,len(matr)):
-        # rotation for corners
+def rotate_matrix_in(matr):
+    print("In place approach: \n", np.matrix(matr))
+    n = len(matr)
+    for i in range(n): # i are rows
         for j in range(i):
-            #rotation  for centers
-            matr[i][j], matr[i][-1] = matr[i][-1], matr[i][0]
-            matr[i][1], matr[j][1] = matr[j][1], matr[i][1]
-    print(matr)
+            matr[i][j], matr[j][i] = matr[j][i], matr[i][j]
+    print(np.matrix(matr))
+
+# rotate_matrix_in(matr)
+
+
+def rotate_matrix(matr):
+    print("Using a second matrix: \n", np.matrix(matr))
+    new_arr = [[0,0,0],[0,0,0],[0,0,0]]
+    n = len(matr)
+    for i in range(n): # i are rows
+        for j in range(n):
+            new_arr[i][j] = matr[j][j]
+    print("New array: \n", np.matrix(new_arr))
 
 rotate_matrix(matr)
